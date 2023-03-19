ea6b5ca0083985c410d748ab702c44ba9e3d4631
tr1ten <shubhi.blog@gmail.com>
2023-03-19 23:47:32 +0530

    completed lab3 work

--- experiments/SimulatingLAN/exp6.tcl
+++ experiments/SimulatingLAN/exp6.tcl
@@ -14,34 +14,27 @@ proc finish {} {
         exit 0
         }
 
-#creating Nodes        
-for {set i 0} {$i<5} {incr i} {
+#creating ns        
+for {set i 0} {$i<10} {incr i} {
 set n($i) [$ns node]
 }
 
 #Creating Links
-for {set i 0} {$i<4} {incr i} {
-$ns duplex-link $n(4) $n($i) 512Kb 10ms DropTail
+for {set i 1} {$i<10} {incr i} {
+    $ns simplex-link $n([expr $i-1]) $n($i) 1Mb 10ms DropTail
 }
-
-#Orienting The nodes
-$ns duplex-link-op $n(4) $n(0) orient left-up
-$ns duplex-link-op $n(4) $n(1) orient right-up
-$ns duplex-link-op $n(4) $n(2) orient left-down
-$ns duplex-link-op $n(4) $n(3) orient right-down
+$ns simplex-link $n(9) $n(0) 1Mb 10ms DropTail
 
 # #TCP_Config
 set tcp0 [new Agent/TCP]
 $ns attach-agent $n(0) $tcp0
 
-$tcp0 set class_ 1
 $tcp0 set fid_ 0
 
 set sink0 [new Agent/TCPSink]
 $ns attach-agent $n(3) $sink0
 
 $ns connect $tcp0 $sink0
-
 
 # #FTP Config
 set ftp0 [new Application/FTP]
--- learn/cheatsheet.tcl
+++ learn/cheatsheet.tcl
@@ -1,7 +1,7 @@
 set a 12
-set b [expr $a + 21]
-if { $a>10 } {
-	puts "a greator than 10"
-}
-puts $b
-puts $a
+# set b [expr $a + 21]
+# if { $a>10 } {
+# 	puts "a greator than 10"
+# }
+# puts $b-1
+puts [expr $a-1]
